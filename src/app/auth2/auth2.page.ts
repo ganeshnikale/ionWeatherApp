b7695d6f54fa790a8931dca18c827be53a916978
ganesh nikale <ganesh.nikale@wwindia.com>
2019-11-05 15:01:12 +0530

    firebase loginWithEmailPass

--- src/app/auth2/auth2.page.ts
+++ src/app/auth2/auth2.page.ts
@@ -1,6 +1,9 @@
 import { auth } from 'firebase';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import {AngularFireAuth } from 'angularfire2/auth';
+
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-auth2',
@@ -8,21 +11,42 @@ import {AngularFireAuth } from 'angularfire2/auth';
   styleUrls: ['./auth2.page.scss'],
 })
 export class Auth2Page implements OnInit {
-
+  newOfferForm: FormGroup;
   constructor(private afAuth:AngularFireAuth) { }
 
   ngOnInit() {
-    
-    this.afAuth.auth.createUserWithEmailAndPassword('ganeshnikale@gmail.com', '1234ganesh').then( data =>{
-      console.log(data)
-    });
+    this.loginCredintials();
+    // this.afAuth.auth.createUserWithEmailAndPassword('ganeshnikale@gmail.com', '1234ganesh').then( data =>{
+    //   console.log(data)
+    // });
     
   }
-
-  signIn(){
-    this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider()).then(data =>{
-      console.log(data);
-    })
+  loginCredintials(){
+    console.log('aaa');
+    this.newOfferForm = new FormGroup({
+      userName : new FormControl(null,{
+        updateOn:'blur',
+        validators: [Validators.required,Validators.minLength(4)]
+      }),
+      Passwords : new FormControl(null,{
+        updateOn: 'blur',
+       validators: [Validators.required,Validators.minLength(4)]
+      }),
+      
+    });
+    console.log(this.newOfferForm);
+    console.log(this.newOfferForm.value.userName);
+  }
+  login(){
+    console.log(this.newOfferForm);
+    console.log(this.newOfferForm.value.userName);
+    console.log(this.newOfferForm.value.Passwords);
+    this.afAuth.auth.signInWithEmailAndPassword(this.newOfferForm.value.userName,this.newOfferForm.value.Passwords).then( data =>{
+      console.log(data)
+    }).catch( error =>{
+      console.log(error);
+    });
+  // this.afAuth.auth.createUserWithEmailAndPassword('ganeshnikale@gmail.com','ganesh@123');
   }
 
 }
